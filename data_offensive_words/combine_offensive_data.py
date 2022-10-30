0ed6122cc0fccf6660f352d5dc64809fe9ff9f0d
TessaZwart <tessa-zwart@hotmail.com>
2022-10-30 11:24:41 +0100

    Delete the offensive words that we are not using

--- data_offensive_words/combine_offensive_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-bad_words_df_1 = pd.read_csv("bad-bad-words.csv", names=['offensive_word'])
-
-bad_words_df_2 = pd.read_csv("luis_von_ahn_badwords.csv", names=['offensive_word'])
-
-combined_words = pd.concat([bad_words_df_1,bad_words_df_2]).drop_duplicates().reset_index(drop=True)
-print()
