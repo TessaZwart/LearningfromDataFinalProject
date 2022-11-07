95d4ccada0dc2908a3b05f64e2ab4c7b4d9b0c70
TessaZwart <63647830+TessaZwart@users.noreply.github.com>
2022-11-07 15:57:13 +0100

    Update baseline, added args parser

--- baseline.py
+++ baseline.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import time
+import argparse
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
@@ -13,10 +14,11 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm
 
 from evaluation import evaluate_model
-from preprocessing import clean_dataframe
+#from preprocessing import clean_dataframe
 from preprocessing import data_vectorizer
 
-np.random.seed(1234)
+np.random.seed(101010101)
+print('random seeds: 101010101')
 
 
 def logistic_regression(x_train, y_train, x_test, y_test, le):
@@ -114,18 +116,25 @@ def print_all_baseline_results(x_train, y_train, x_test, y_test, le):
 
 
 if __name__ == "__main__":
-    df_train = pd.read_csv('preprocessed_data/train.csv', names=['text', 'label'])
-    df_dev = pd.read_csv('preprocessed_data/dev.csv', names=['text', 'label'])
-    df_test = pd.read_csv('preprocessed_data/test.csv', names=['text', 'label'])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-tf", "--train_file", default='preprocessed_data/train.csv', type=str,
+                        help="Train file to learn from (default train.csv)")
+    parser.add_argument("-df", "--dev_file", default='preprocessed_data/dev.csv', type=str,
+                        help="Dev file to evaluate on (default dev.csv)")
+    parser.add_argument("-tsf", "--test_file", default='preprocessed_data/test.csv', type=str,
+                        help="Test file to evaluate on (default test.csv)")
+    parser.add_argument("-sw", "--stop_words", default=False, type=bool,
+                        help="Remove stopwords True/False (default false)")
+    args = parser.parse_args()
+    
+    df_train = pd.read_csv(args.train_file, names=['text', 'label'])
+    df_dev = pd.read_csv(args.dev_file, names=['text', 'label'])
+    df_test = pd.read_csv(args.test_file, names=['text', 'label'])
 
     df_train['text'] = df_train['text'].astype(str)
     df_dev['text'] = df_dev['text'].astype(str)
     df_test['text'] = df_test['text'].astype(str)
 
-    # Preprocessing the data
-    df_train['text'] = clean_dataframe(df_train['text'])
-    df_test['text'] = clean_dataframe(df_test['text'])
-    df_dev['text'] = clean_dataframe(df_dev['text'])
 
     x_train, y_train, x_test, y_test, le = data_vectorizer(df_train['text'], df_train['label'], df_test['text'],
                                                            df_test['label'])
