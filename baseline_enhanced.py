1ce6083f9fcbfe8cb685d5af7d4ca0088c5cff27
TessaZwart <63647830+TessaZwart@users.noreply.github.com>
2022-11-07 21:15:20 +0100

    Update baseline_enhanced.py

--- baseline_enhanced.py
+++ baseline_enhanced.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
-
-"""Uses multiple sklearn ML algorithms to classify review categories based on their textual contents."""
-
 import time
 import spacy
 import pandas as pd
+import argparse
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -19,11 +16,11 @@ from preprocessing import clean_dataframe
 spacy.load("en_core_web_sm")
 
 
+
 def naive_bayes(features, x_train, y_train, x_test, x_dev, y_dev):
     """Run Naive Bayes classification on an input vector."""
     classifier = Pipeline([('features', features), ('cls', MultinomialNB(alpha=1, fit_prior=True))])
     classifier.fit(x_train, y_train)
-    #xxx = tune_parameters(classifier, x_dev, y_dev)
     y_pred = classifier.predict(x_test)
     return y_pred
 
@@ -32,7 +29,7 @@ def tune_parameters(classifier, x_dev, y_dev):
     """" Find the best parameters using GridSearchCV"""
     # Create a dictionary of possible parameters
     print("Tuning parameters. This might take a while...")
-    params_grid = {'cls__alpha': [0.00001, 0.0001, 0.001, 0.1, 1, 10, 100, 1000],'cls__fit_prior': [True, False]}
+    params_grid = {'cls__alpha': [0.00001, 0.0001, 0.001, 0.1, 1],'cls__fit_prior': [True, False]}
 
     # Create the GridSearchCV object
     grid_clf = GridSearchCV(estimator=classifier, param_grid=params_grid, scoring='accuracy')
@@ -53,35 +50,52 @@ def construct_features():
     """Constructs a FeatureUnion of multiple features."""
     # Unigram/Bigram/Trigram
     uni_bi_trigram = CountVectorizer(analyzer='word', ngram_range=(1, 3), token_pattern=r'\b\w+\b', max_features=5000)
-    # TFIDF
-    tfidf = TfidfVectorizer(analyzer='word', token_pattern=r'\b\w+\b', max_features=5000)
     # Part-Of-Speech tags
     pos = CountVectorizer(tokenizer=get_POS_tags, analyzer='word', token_pattern=r'\b\w+\b', max_features=5000)
     # 3-6 character N-grams
     ngram = CountVectorizer(analyzer='char_wb', ngram_range=(3, 6), min_df=3)
 
     # Construct a union of features
-    union = FeatureUnion([('uni_bi_trigram', uni_bi_trigram), ('tfidf', tfidf), ('pos', pos), ('ngram', ngram)])
+    union = FeatureUnion([('uni_bi_trigram', uni_bi_trigram), ('pos', pos), ('ngram', ngram)])
     return union
 
 
 if __name__ == "__main__":
     # Load SpaCy language model
     nlp = spacy.load('en_core_web_sm')
-
-    # Load the files
-    df_train = pd.read_csv('preprocessed_data/train.tsv', sep='\t', names=['text', 'label'])
-    df_dev = pd.read_csv('preprocessed_data/dev.tsv', sep='\t', names=['text', 'label'])
-    df_test = pd.read_csv('preprocessed_data/test.tsv', sep='\t', names=['text', 'label'])
+    
+    # Parser arguments to make it easier to use different data
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-tf", "--train_file", default='preprocessed_data/train.csv', type=str,
+                        help="Train file to learn from (default train.csv)")
+    parser.add_argument("-df", "--dev_file", default='preprocessed_data/dev.csv', type=str,
+                        help="Dev file to evaluate on (default dev.csv)")
+    parser.add_argument("-tsf", "--test_file", default='preprocessed_data/test.csv', type=str,
+                        help="Test file to evaluate on (default test.csv)")
+    parser.add_argument("-sw", "--stop_words", default=False, type=bool,
+                        help="Remove stopwords True/False (default false)")
+    args = parser.parse_args()
+    
+    df_train = pd.read_csv(args.train_file, sep=',', names=['text', 'label'])
+    df_dev = pd.read_csv(args.dev_file, sep=',', names=['text', 'label'])
+    df_test = pd.read_csv(args.test_file, sep=',', names=['text', 'label'])
+    
+    # Deleting empty data
+    df_train.dropna()
+    df_dev.dropna()
+    df_test.dropna()
 
     df_train['text'] = df_train['text'].astype(str)
     df_dev['text'] = df_dev['text'].astype(str)
     df_test['text'] = df_test['text'].astype(str)
+    
+    
 
     # Construct a set of features
     features = construct_features()
 
     naive_bayes_pred = naive_bayes(features, df_train['text'], df_train['label'], df_test['text'],
                                                    df_dev['text'], df_dev['label'])
-
+    
+    # Give the evaluation of the model
     print(evaluate_model(df_test['label'], naive_bayes_pred))
